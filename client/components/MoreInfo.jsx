08bc2928f65a551a50121ded76b8aa0565baf3ea
zoeIML <zoe.lisk@gmail.com>
2018-04-13 07:56:13 +1200

    add headings in moreinfo

--- client/components/MoreInfo.jsx
+++ client/components/MoreInfo.jsx
@@ -6,6 +6,12 @@ const MoreInfo = () => {
   return (
     <div className="more-info">
       <h1>More Info</h1>
+      <h2>Mission Statement</h2>
+      <p>This will be the mission statement</p>
+      <h2>Cruelty-Free Sources</h2>
+      <p>This is where I talk about where I get my info from</p>
+      <h2>Want in?</h2>
+      <p>This will be info about participating</p>
     </div>
   )
 }
